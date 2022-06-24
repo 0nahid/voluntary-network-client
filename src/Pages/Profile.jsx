e67d7d472b5960ce499f2c894e8430838b84ba5f
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-06-25 02:19:22 +0600

    added profile page

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../firebase.init';
 
 export default function Profile() {
+  const [user] = useAuthState(auth);
+  console.log(user);
   return (
-    <div>Profile</div>
+    <div>
+      <div class="card w-96 mx-auto glass">
+        <figure><img src={user.photoURL} alt={user.displayName} /></figure>
+        <div class="card-body">
+          <h2 class="card-title font-bold">Name:{user.displayName}</h2>
+          <p>Email: {user.email}</p>
+          <p>Last Login: {user.metadata.lastSignInTime}</p>
+        </div>
+      </div>
+    </div>
   )
 }
