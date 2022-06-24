e2477c65b050cd5f8d597cfcb219a6ba5cb9c35c
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-06-24 16:58:30 +0600

    initialized voluntary network client & added react-daisyui

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Button } from 'react-daisyui';
+import './index.css';
+export default function App() {
+    return (
+        <div>
+            <Button className='text-red-600 '>Click me!</Button>
+            <Button color="primary">Click me!</Button>
+        </div>
+    )
+}
